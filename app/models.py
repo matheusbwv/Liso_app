afd76de884460bf51a3ba0b4517385767cab92d2
matheusbwv <matheus.wenes@gmail.com>
2024-08-08 16:20:01 -0300

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class saldo(models.Model):
+  valor = models.FloatField()
+
+class despesa(models.Model):
+  descricao = models.CharField(max_length=120)
+  valor = models.FloatField()
+  categoria = models.SmallIntegerField()
+class receita(models.Model):
+  descricao = models.CharField(max_length=120)
+  valor = models.FloatField()
+  categoria = models.SmallIntegerField()
+  
